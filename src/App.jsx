c591df0cd44dfc479502471af5a7dd44151798d7
Geoffroy Periot <geoffroy.periot@wildcodeschool.fr>
2021-10-26 11:35:56 +0200

    end of dojo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
 import PokemonDetails from './components/PokemonDetails';
-import { magikarp } from './pokemon';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(false);
+  const getPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${search}/`)
+      .then((res) => res.data)
+      .then(data => ({
+        id: data.id,
+        name: data.name,
+        image: data.sprites.front_default,
+        types: data.types,
+      }))
+      .then((data) => {
+        setPokemon(data);
+        setError(false)
+      })
+      .catch((err) => {
+        setError(true)
+      })  
+  };
+
   return (
     <div className='App'>
       <h1>Pokedex</h1>
       <label htmlFor='search'>
         Search pokemon
-        <input id='search' type='text' />
+        <input id='search' type='text' value={search} onChange={event => setSearch(event.target.value)} />
       </label>
-      <PokemonDetails pokemon={magikarp} />
+      {error && <p>This pokemon doesn't exist</p>}
+      <button onClick={getPokemon}>click here</button>
+      {pokemon && <PokemonDetails {...pokemon} />}
     </div>
   );
 }
