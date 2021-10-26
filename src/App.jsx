274aeeec5f843df6b6d434eef96b1f4ee752cbca
Geoffroy Periot <geoffroy.periot@wildcodeschool.fr>
2021-10-26 12:39:06 +0200

    end of dojo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import PokemonDetails from './components/PokemonDetails';
-import { magikarp } from './pokemon';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import PokemonDetails from './components/PokemonDetails';
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(false);
+
+  const getPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+      .then((resp) => resp.data)
+      .then((data) => ({
+        id: data.id,
+        name: data.name,
+        image: data.sprites.front_default,
+        types: data.types,
+      }))
+      .then((p) => {
+        setPokemon(p)
+        setError(false)
+      })
+      .catch((err) => {
+        setPokemon(null)
+        setError(true)
+      })
+  };
+
   return (
     <div className='App'>
       <h1>Pokedex</h1>
       <label htmlFor='search'>
         Search pokemon
-        <input id='search' type='text' />
+        <input id='search' type='text' value={search} onChange={e => setSearch(e.target.value)}/>
+        <button onClick={getPokemon}>search</button>
       </label>
-      <PokemonDetails pokemon={magikarp} />
+      {error && <p>This pokemon doesn't exist</p>}
+      {pokemon && <PokemonDetails {...pokemon}/>}
     </div>
   );
 }
--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,24 +1,18 @@
 import './PokemonDetails.css';
 
-const PokemonDetails = ({ pokemon }) => (
+const PokemonDetails = ({ name, id, image, types }) => ( 
   <div className='pokemon-container'>
-    <img src="" alt='' className='pokemon-image' />
+    <img src={image} alt={name} className='pokemon-image' />
     <div className='pokemon-header'>
-      <h3>
-        {/* mettre ici le nom du pokemon */}
-      </h3>
+      <h3>{name}</h3>
     </div>
     <div className='pokemon-body'>
       <div>
         <h4>Types:</h4>
         <ul>
-          {/* Mettre ici la liste des types du pokemon */}
-        </ul>
-      </div>
-      <div>
-        <h4>Moves:</h4>
-        <ul>
-          {/* Mettre ici la liste des attaques du pokemon */}
+          {types.map((type) => (
+            <li>{type.type.name}</li>
+          ))}
         </ul>
       </div>
     </div>
